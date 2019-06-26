b0266cbcb8ab5c8204c00419b28c7f4362e44b20
Antoine van der Lee <antoine@wetransfer.com>
2019-06-26 18:03:15 +0200

    Rules of a subscription have been added

--- Combine.playground/Pages/Rules of subscriptions.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Rules of subscriptions.xcplaygroundpage/Contents.swift
@@ -1,8 +1,36 @@
 /*:
  [Previous](@previous)
  ## The rules of a subscription
- - There can only be _one_ subscriber
+ - A subscriber will receive a _single_ subscription
  - _Zero_ or _more_ values can be published
  - At most _one_ completion will be called
  */
+import Combine
+import UIKit
+
+enum ExampleError: Swift.Error {
+    case somethingWentWrong
+}
+
+let subject = PassthroughSubject<String, ExampleError>()
+
+//let publisher = Publisher<String, ExampleError>(subject: subject)
+subject.handleEvents(receiveSubscription: { (subscription) in
+    print("New subscription!")
+}, receiveOutput: { _ in
+    print("Received new value!")
+}, receiveCompletion: { _ in
+    print("A subscription completed")
+}, receiveCancel: {
+    print("A subscription cancelled")
+}).sink { (value) in
+    print("Subscriber one received value: \(value)")
+}
+
+subject.send("Hello!")
+subject.send("Hello again!")
+subject.send("Hello for the last time!")
+subject.send(completion: Subscribers.Completion<ExampleError>.failure(ExampleError.somethingWentWrong))
+subject.send("Hello?? :(")
+
 //: [Next](@next)
