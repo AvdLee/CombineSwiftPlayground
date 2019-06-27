fe885a0b080bea4dca2012a38612d08d3346e775
Antoine van der Lee <antoine@wetransfer.com>
2019-06-27 15:43:55 +0200

    Better explain subscribers, operators and publishers

--- Combine.playground/Pages/Publishers & Subscribers.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Publishers & Subscribers.xcplaygroundpage/Contents.swift
@@ -2,13 +2,22 @@ import Foundation
 import Combine
 /*:
  [Previous](@previous)
- ## Publishers and Subscribers
+ ## Publishers, Operators, and Subscribers
  - A Publisher exposes values that can change on which..
+ - .. change through operators and ..
  - ..a subscriber subscribes to receive all those updates
 
  */
 let publisher = Publishers.Just(28)
-publisher.sink { (value) in
-    print("Subscriber received value: \(value)")
-}
+
+// This creates a `Subscriber` on the `Just a 28` Publisher
+publisher
+    // We change the value
+    .map { number in
+        return "Antoine's age is \(number)"
+    }
+    // We subscribe using `sink`
+    .sink { (value) in
+        print(value)
+    }
 //: [Next](@next)
