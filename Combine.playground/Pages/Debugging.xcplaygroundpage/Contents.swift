42342bf94e38321d4189d88c63d1f56472b7a6e4
Antoine van der Lee <antoine@wetransfer.com>
2019-07-08 21:41:53 +0200

    Add a page for debugging

---
+++ Combine.playground/Pages/Debugging.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+import Foundation
+import UIKit
+import Combine
+/*:
+ [Previous](@previous)
+ ## Debugging
+ Operators which help to debug Combine streams and implementations.
+ */
+
+enum ExampleError: Swift.Error {
+    case somethingWentWrong
+}
+
+/*:
+ #### Handling events
+ Can be used combined with breakpoints for further insights.
+ */
+let subject = PassthroughSubject<String, ExampleError>()
+let subscription = subject.handleEvents(receiveSubscription: { (subscription) in
+    print("Receive subscription")
+}, receiveOutput: { output in
+    print("Received output: \(output)")
+}, receiveCompletion: { _ in
+    print("Receive completion")
+}, receiveCancel: {
+    print("Receive cancel")
+}, receiveRequest: { demand in
+    print("Receive request: \(demand)")
+}).sink { _ in }
+
+subject.send("Hello!")
+subscription.cancel()
+
+// Prints out:
+// Receive request: unlimited
+// Receive subscription
+// Received output: Hello!
+// Receive cancel
+
+//subject.send(completion: .finished)
+
+/*:
+ #### Print
+ Using the print operator to log messages for all publishing events.
+ */
+
+let printSubscription = subject.print("Print example").sink { _ in }
+
+subject.send("Hello!")
+printSubscription.cancel()
+
+// Prints out:
+// Print example: receive subscription: (PassthroughSubject)
+// Print example: request unlimited
+// Print example: receive value: (Hello!)
+// Print example: receive cancel
+
+//: [Next](@next)
