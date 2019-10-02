4ac3ae6317f2d4d7f8e5b9c151c76ac99e8f267b
Florent Pillet <fpillet@gmail.com>
2019-10-02 19:51:43 +0200

    Rewording

--- Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -5,19 +5,22 @@ import UIKit
 /*:
  [Previous](@previous)
  ## Foundation and Combine
- Foundation adds Combine implementations to standard types, like:
+ Foundation adds Combine publishers for many types, like:
  */
 /*:
  ##### A URLSessionTask publisher and a JSON Decoding operator
  */
 struct DecodableExample: Decodable { }
+
 URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.avanderlee.com/feed/")!)
     .map { $0.data }
     .decode(type: DecodableExample.self, decoder: JSONDecoder())
+
 /*:
  ##### A Publisher for notifications
  */
 NotificationCenter.default.publisher(for: .NSSystemClockDidChange)
+
 /*:
  ##### KeyPath binding to NSObject instances
  */
