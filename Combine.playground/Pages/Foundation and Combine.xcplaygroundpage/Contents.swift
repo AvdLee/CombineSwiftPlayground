31c94335c59f48a35178d8ae9647e0966718f577
Florent Pillet <fpillet@gmail.com>
2019-11-30 22:12:20 +0100

    'Foundation and Combine' page updated

--- Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -1,14 +1,16 @@
+//: [Previous](@previous)
+
 import Foundation
 import Combine
 import UIKit
 
 /*:
- [Previous](@previous)
  ## Foundation and Combine
  Foundation adds Combine publishers for many types, like:
  */
+
 /*:
- ##### A URLSessionTask publisher and a JSON Decoding operator
+ ### A URLSessionTask publisher and a JSON Decoding operator
  */
 struct DecodableExample: Decodable { }
 
@@ -17,15 +19,25 @@ URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.avanderlee.com
     .decode(type: DecodableExample.self, decoder: JSONDecoder())
 
 /*:
- ##### A Publisher for notifications
+ ### A Publisher for notifications
  */
 NotificationCenter.default.publisher(for: .NSSystemClockDidChange)
 
 /*:
- ##### KeyPath binding to NSObject instances
+ ### KeyPath binding to NSObject instances
  */
 let ageLabel = UILabel()
 Just(28)
     .map { "Age is \($0)" }
     .assign(to: \.text, on: ageLabel)
+
+/*:
+### A Timer publisher exposing Cocoa's `Timer`
+- this one is a bit special as it is a `Connectable`
+- ... use `autoconnect` to automatically start it when a subscriber subscribes
+*/
+let publisher = Timer
+	.publish(every: 1.0, on: .main, in: .common)
+	.autoconnect()
+
 //: [Next](@next)
